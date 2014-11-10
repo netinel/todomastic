5e3374b2fed48f2e14f61fa7da5907352a036b94
Dmytro <netinel@gmail.com>
2014-11-11 01:04:52 +0200

    added navigation bar

---
+++ src/js/events/events.js
@@ -0,0 +1,11 @@
+var todoMastic = todoMastic || {};
+todoMastic.models = todoMastic.models || {};
+todoMastic.views = todoMastic.views || {};
+todoMastic.collections = todoMastic.collections || {};
+todoMastic.events = todoMastic.events || {};
+
+(function(){
+
+    todoMastic.events = _.extend({}, Backbone.Events);
+
+}());
---
+++ src/js/views/navigation.js
@@ -0,0 +1,19 @@
+var todoMastic = todoMastic || {};
+todoMastic.models = todoMastic.models || {};
+todoMastic.views = todoMastic.views || {};
+todoMastic.collections = todoMastic.collections || {};
+todoMastic.events = todoMastic.events || {};
+
+(function(){
+
+    var navigationView = Backbone.View.extend({
+
+        el: '#todo-nav',
+
+        initialize: function(){
+
+        }
+
+    })
+
+}());
